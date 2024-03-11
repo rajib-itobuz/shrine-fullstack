584c44e22cfad63389d1ab1803d630c1308ca8f6
Rajib Nandi <rajib-itobuz.com>
2024-03-12 03:41:05 +0530

    fix: header & hero section

--- helper/helper.js
+++ helper/helper.js
@@ -23,3 +23,131 @@ export const cartWidget = () => {
     cart.append(cartIconWrapper);
 
 }
+
+export const createHeading = (headingElement, headingObject) => {
+    const heading = document.createElement("h4");
+    heading.setAttribute("class", "fs-1 border-3 ps-3 border-danger border-start w-100");
+    heading.innerHTML = headingObject.heading;
+
+
+    const subHeading = document.createElement("h5");
+    subHeading.setAttribute("class", "fs-6");
+    subHeading.innerText = headingObject.subHeading;
+
+    headingElement.append(heading, subHeading);
+}
+
+export const createEventCard = ({ imgUrl, title, subtitle, button, date, location, time, imgCustomClass = '', bodyCustomClass = '', cardCustomClass = '' }) => {
+
+    const card = document.createElement("div");
+    card.setAttribute("class", `card mb-3 flex-shrink-0 ${cardCustomClass}`);
+
+
+
+    const rowWrapper = document.createElement("div");
+    rowWrapper.setAttribute("class", "row g-0");
+
+    const imageWrapper = document.createElement("div");
+    imageWrapper.setAttribute("class", "col-12 col-sm-5");
+
+
+    const image = document.createElement("img");
+    image.src = imgUrl;
+    image.setAttribute("class", `w-100 rounded-start object-fit-cover ${imgCustomClass}`);
+    image.alt = title;
+
+
+    const cardBodyWrapper = document.createElement("div");
+    cardBodyWrapper.setAttribute("class", "col-12 col-sm-7");
+
+    const cardBody = document.createElement("div");
+    cardBody.setAttribute("class", `card-body ${bodyCustomClass}`);
+
+    if (date) {
+        const dateElement = document.createElement("h6");
+        dateElement.textContent = date;
+        cardBody.append(dateElement);
+    }
+    if (location) {
+        const locationElement = document.createElement("h6");
+        locationElement.textContent = location;
+        cardBody.append(locationElement);
+    }
+    if (time) {
+        const timeElement = document.createElement("h6");
+        timeElement.textContent = time;
+        cardBody.append(timeElement);
+    }
+
+    const cardTitle = document.createElement("h5");
+    cardTitle.innerHTML = title;
+    cardTitle.setAttribute("class", "card-title");
+
+    const cardText = document.createElement("p");
+    cardText.innerHTML = subtitle;
+    cardText.setAttribute("class", "card-text");
+    cardBody.append(cardTitle, cardText);
+
+
+    if (button) {
+        const buttonElement = document.createElement("button");
+        buttonElement.textContent = button;
+        buttonElement.setAttribute("class", "btn btn-outline-dark")
+        cardBody.append(buttonElement);
+    }
+
+
+    cardBodyWrapper.append(cardBody);
+    imageWrapper.append(image);
+    rowWrapper.append(imageWrapper, cardBodyWrapper);
+    card.append(rowWrapper);
+
+    return card;
+
+}
+
+export const createBlogCard = ({ imgUrl, title, subtitle, userImgUrl,readTime, imgCustomClass = '', bodyCustomClass = '', cardCustomClass = '' }) => {
+
+    const cardWrapper = document.createElement('div');
+    cardWrapper.setAttribute('class', 'card flex-shrink-0');
+    cardWrapper.setAttribute('style', 'max-width: 330px;aspect-ratio:1');
+
+    const cardImage = document.createElement('img');
+    cardImage.src=imgUrl
+    cardImage.setAttribute('class', `card-img-top ${imgUrl}`);
+    cardImage.alt=title;
+
+    const cardBody = document.createElement('div');
+    cardBody.setAttribute('class', 'card-body');
+
+    const cardTitle = document.createElement('h5');
+    cardTitle.setAttribute('class', 'card-title');
+    cardTitle.innerHTML=title;
+
+
+    const cardText = document.createElement('p');
+    cardText.setAttribute('class', 'card-text py-3');
+    cardText.innerHTML=subtitle;
+
+
+    const userDiv = document.createElement('div');
+    userDiv.setAttribute('class', 'd-flex align-items-center');
+
+
+    const userImg=document.createElement("img");
+    userImg.src=userImgUrl;
+    userImg.alt="user";
+
+    const readTimeText=document.createElement("p");
+    readTimeText.textContent=readTime;
+    readTimeText.setAttribute("class","m-0")
+    
+    
+    userDiv.append(userImg,readTimeText);
+    cardBody.append(cardTitle,cardText,userDiv);
+    cardWrapper.append(cardImage,cardBody);
+
+    
+    return cardWrapper;
+
+}
--- script.js
+++ script.js
@@ -1,5 +1,101 @@
-import { cartWidget } from './helper/helper.js'
+import { cartWidget, createBlogCard, createEventCard, createHeading } from './helper/helper.js';
 
 
+const headings=document.querySelectorAll(".heading");
+
+const sectionHeaders = [
+    {
+        heading: "Your Satisfaction is<br/> your business",
+        subHeading: "A long established fact that a reader will be distracted by the readable content of a page when looking.A long established fact that.A long."
+    },
+    {
+        heading: "Feel god closer to<br> your members",
+        subHeading: "A long established fact that a reader will be distracted by the readable content of a page when looking.A long established fact that.A long."
+    },
+    {
+        heading: "See our all<br>events",
+        subHeading: "A long established fact that a reader will be distracted by the readable content of a page when looking.A long established fact that.A long.",
+        eventCardObject:[
+            {
+                imgUrl:'./images/events/events.png',
+                title:'Mindfullness Meidtation',
+                subtitle:'A long established fact that a reader will be distracted by the readable content of...',
+                date:'24.12.2023-28.12.2023',
+                location:'Russian Federation St. Peter\'s Church',
+                time:'4:38-8:24',
+                button:'Join Now',
+            },
+            {
+                imgUrl:'./images/events/events.png',
+                title:'The positive aura seminar',
+                subtitle:'A long established fact that a reader will be distracted by the readable content of...',
+                date:'24.12.2023-28.12.2023',
+                location:'Russian Federation St. Peter\'s Church',
+                time:'4:38-8:24',
+                button:'Join Now',
+            },
+            {
+                imgUrl:'./images/events/events.png',
+                title:'Mindfullness Meidtation',
+                subtitle:'A long established fact that a reader will be distracted by the readable content of...',
+                date:'24.12.2023-28.12.2023',
+                location:'Russian Federation St. Peter\'s Church',
+                time:'4:38-8:24',
+                button:'Join Now',
+            }
+        ]
+    },{
+        heading: "Our latest<br>Articles",
+        subHeading: "A long established fact that a reader will be distracted by the readable content of a page when looking.A long established fact that.A long.",
+        blogCards:[
+            {
+                imgUrl:'./images/articles/church.png',
+                title:'Baptism Sunday of<br/> church',
+                subtitle:'Sir Gerry Serrano A long established fact that a reader will be distracted by the readable content of...',
+                userImgUrl:'./images/subscribe/mary.png',
+                readTime:'2min read'
+            },
+            {
+                imgUrl:'./images/articles/church.png',
+                title:'Baptism Sunday of<br/> church',
+                subtitle:'Sir Gerry Serrano A long established fact that a reader will be distracted by the readable content of...',
+                userImgUrl:'./images/subscribe/mary.png',
+                readTime:'2min read'
+            },
+            {
+                imgUrl:'./images/articles/church.png',
+                title:'Baptism Sunday of<br/> church',
+                subtitle:'Sir Gerry Serrano A long established fact that a reader will be distracted by the readable content of...',
+                userImgUrl:'./images/subscribe/mary.png',
+                readTime:'2min read'
+            }
+        ]
+    }
+]
 
 cartWidget();
+
+
+headings.forEach((headingElement,index)=>{
+    createHeading(headingElement,sectionHeaders[index]);
+    const eventCards=sectionHeaders[index].eventCardObject;
+    
+    
+    
+    if(eventCards){
+        const cardContainer=headingElement.parentElement.querySelector('.cardContainer');
+        
+        eventCards.forEach(e=>{
+            cardContainer.append(createEventCard({...e,imgCustomClass:'event-image',cardCustomClass:'event-card'}))
+        })
+    }
+
+
+    const blogCards=sectionHeaders[index].blogCards;
+    if(blogCards){
+        const cardContainer=headingElement.parentElement.querySelector(`.cardContainer`);
+        blogCards.forEach(e=>{
+            cardContainer.append(createBlogCard({...e}))
+        })
+    }
+})
